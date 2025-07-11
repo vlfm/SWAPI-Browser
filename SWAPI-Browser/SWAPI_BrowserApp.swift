99537c87836a3af05e490765dff57e9ac5100a55
Valery Fomenko <valery.fomenko.vlfm@gmail.com>
2025-07-11 12:41:44 +0100

    Create project

---
+++ SWAPI-Browser/SWAPI_BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWAPI_BrowserApp.swift
+//  SWAPI-Browser
+//
+//  Created by Valery Fomenko on 11/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct SWAPI_BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SWAPI-BrowserTests/SWAPI_BrowserTests.swift
@@ -0,0 +1,17 @@
+//
+//  SWAPI_BrowserTests.swift
+//  SWAPI-BrowserTests
+//
+//  Created by Valery Fomenko on 11/07/2025.
+//
+
+import Testing
+@testable import SWAPI_Browser
+
+struct SWAPI_BrowserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
